1ae787bd789e02b741d888486738eb6df2b5b457
Luca Poli <poli.luca.marconi@gmail.com>
2024-02-22 09:50:52 +0100

    heroku deployment p11

--- constants.py
+++ constants.py
@@ -1,11 +1,13 @@
 import os
 
-PROJECT_NAME = "app"
-PROJECT_PATH = os.getcwd()
+# PROJECT_NAME = "app"
+# PROJECT_PATH = os.getcwd()
+#
+# while os.path.basename(os.getcwd()) != PROJECT_NAME:
+#     os.chdir("..")
+#     PROJECT_PATH = os.getcwd()
 
-while os.path.basename(os.getcwd()) != PROJECT_NAME:
-    os.chdir("..")
-    PROJECT_PATH = os.getcwd()
+PROJECT_PATH = os.getcwd()
 
 DATA_PATH = os.path.join(PROJECT_PATH, "static", "data")
 OUTPUT_PATH = os.path.join(DATA_PATH, "output")
